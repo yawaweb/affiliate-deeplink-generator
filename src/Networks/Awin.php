bae113e6d02918a0fe8405af677b4d014c081669
Ousama Yamine <35897222+yawaweb@users.noreply.github.com>
2021-05-21 02:10:03 +0200

    Create Awin.php

---
+++ src/Networks/Awin.php
@@ -0,0 +1,83 @@
+<?php
+namespace yawaweb\AffiliateDeeplinkGenerator\Networks;
+
+class Awin
+{
+    /**
+     * Affiliate Domain
+     */
+    const AFFILIATE_DOMAIN = 'https://www.awin1.com/cread.php?';
+
+    /**
+     * Generate affiliate url for Awin
+     * Description of url parameter: https://wiki.awin.com/index.php/Deeplink_Builder
+     *
+     * @param int $publisherId
+     * @param int $advertiserId
+     * @param string $deeplink
+     * @param string $clickRef
+     * @param string $viewRef
+     * @param string $pRef
+     * @return string
+     */
+    public function generate($publisherId, $advertiserId, $deeplink, $clickRef = null, $viewRef = null, $pRef = null)
+    {
+        $url_params_array = [];
+        $url_params_array['awinmid'] = $advertiserId; //ADVERTISER_ID
+        $url_params_array['awinaffid'] = $publisherId; //PUBLISHER_ID
+
+        /**
+         * Publisher Reference (available for clicks and views)
+         * Max.: 6
+         */
+        if($pRef != null){
+            if(is_array($pRef)) {
+                foreach($pRef as $key => $value){
+                    $keyForRef = $key+1;
+                    $url_params_array['pref' . $keyForRef] = $value;
+                }
+            }
+            else{
+                $url_params_array['pref1'] = $pRef;
+            }
+        }
+
+        /**
+         * Click Reference (only available for clicks)
+         * Max.: 6
+         */
+        if($clickRef != null){
+            if(is_array($clickRef)) {
+                foreach($clickRef as $key => $value){
+                    $keyForRef = ($key == 0) ? '' : $key+1;
+                    $url_params_array['clickref' . $keyForRef] = $value;
+                }
+            }
+            else{
+                $url_params_array['clickref'] =$clickRef;
+            }
+        }
+
+        /**
+         * View Reference (only available for views)
+         * Max.: 6
+         */
+        if($viewRef != null){
+            if(is_array($viewRef)) {
+                foreach($viewRef as $key => $value){
+                    $keyForRef = $key+1;
+                    $url_params_array['viewref' . $keyForRef] = $value;
+                }
+            }
+            else{
+                $url_params_array['viewref1'] = $viewRef;
+            }
+        }
+
+        $url_params_array['ued'] = $deeplink; //DEEPLINK
+
+        $url_params = http_build_query($url_params_array);
+
+        return self::AFFILIATE_DOMAIN.$url_params;
+    }
+}
