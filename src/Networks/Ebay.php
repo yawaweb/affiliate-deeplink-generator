6a1b483495ca985b4712d24e6a106956718e4462
Ousama Yamine <35897222+yawaweb@users.noreply.github.com>
2021-05-21 03:12:29 +0200

    Update Ebay.php

--- src/Networks/Ebay.php
+++ src/Networks/Ebay.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Ebay Deeplink Generator for Affiliates
+ *
+ * @package   affiliate-deeplink-generator
+ * @author    Ousama Yamine <hello@yawaweb.com>
+ * @copyright 2016-2021 Yawaweb <hello@yawaweb.com>
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @version   1.0.0
+ * @link      https://yawaweb.com
+ *
+ */
+
 namespace yawaweb\AffiliateDeeplinkGenerator\Networks;
 
 class Ebay{
