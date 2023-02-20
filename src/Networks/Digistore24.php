40f4ba50251fea0e6764ddcd5363eed3f44e8315
Ousama Yamine <35897222+yawaweb@users.noreply.github.com>
2023-02-20 21:10:05 +0100

    Update Digistore24.php
    cleaning.

--- src/Networks/Digistore24.php
+++ src/Networks/Digistore24.php
@@ -4,9 +4,10 @@
  *
  * @package   affiliate-deeplink-generator
  * @author    Ousama Yamine <hello@yawaweb.com>
- * @copyright 2016-2021 Yawaweb <hello@yawaweb.com>
+ * @copyright 2016-2023 Yawaweb <hello@yawaweb.com>
  * @license   http://opensource.org/licenses/MIT MIT Public
- * @version   1.0.0
+ * @version   2.0.0
+ * @since     1.0.0
  * @link      https://yawaweb.com
  *
  */
@@ -15,9 +16,9 @@ namespace yawaweb\AffiliateDeeplinkGenerator\Networks;
 
 class Digistore24
 {
-    const AFFILIATE_DOMAIN = 'https://www.digistore24.com/redir/';
+    public const AFFILIATE_DOMAIN = 'https://www.digistore24.com/redir/';
 
-    public function generate($publisherId, $advertiserId, $clickRef = null)
+    public function generate($publisherId, $advertiserId, $clickRef = null): string
     {
         return self::AFFILIATE_DOMAIN.$advertiserId.'/'.$publisherId.'/'.$clickRef;
     }
