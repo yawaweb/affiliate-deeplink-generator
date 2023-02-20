65cd00e082bff836696bfb86ce58c19570e78bf0
Ousama Yamine <35897222+yawaweb@users.noreply.github.com>
2023-02-20 21:09:22 +0100

    Update Belboon.php
    Major changes on Awin, Belboon and Ebay.

--- src/Networks/Belboon.php
+++ src/Networks/Belboon.php
@@ -4,9 +4,10 @@
  *
  * @package   affiliate-deeplink-generator
  * @author    Ousama Yamine <hello@yawaweb.com>
- * @copyright 2016-2021 Yawaweb <hello@yawaweb.com>
+ * @copyright 2016-2023 Yawaweb <hello@yawaweb.com>
  * @license   http://opensource.org/licenses/MIT MIT Public
- * @version   1.0.2
+ * @version   2.0.0
+ * @since     1.0.0
  * @link      https://yawaweb.com
  *
  */
@@ -15,38 +16,85 @@ namespace yawaweb\AffiliateDeeplinkGenerator\Networks;
 
 class Belboon
 {
+    private int $publisherId;
+    private string $trackingBaseUrl;
+    private string $advertiseId = '';
+    private int $adSpaceId;
+    private int $creativesId;
+    private string|array|null $clickRef = null;
 
     /**
-     * Generate affiliate url for Belboon
-     * Description of url parameter: https://ingenioustechnologies.atlassian.net/wiki/spaces/KB/pages/1440383093/Working+with+deeplinks
-     *
-     * @param string $affiliate_domain ex.: https://luna.r.lafamo.com/ts/
-     * @param int $publisherId Affiliate ID
-     * @param string $advertiseId Externe ID des Advertiser
-     * @param int $adspaceId ID der Werbefläche (z.B.: affiliatesite.com)
-     * @param int $creativesId ID des Werbemittels (z.B.: Deeplink)
-     * @param string $deeplink Url vom Shop
-     * @param string|array $clickRef Klick Referenz wenn nur eins da ist dann String wenn mehrere dann array (['ref1', 'ref2'])
-     * @return string Trackingurl
+     * @param int $publisherId
+     * @param int $adSpaceId  ID der Werbefläche (z.B.: affiliatesite.com)
      */
-    public function generate($affiliate_domain, $publisherId, $advertiseId, $adspaceId, $creativesId, $deeplink, $clickRef = null)
+    public function __construct(int $publisherId, int $adSpaceId)
     {
-        $url_params_array['amc'] = 'con.blbn.'.$publisherId.'.'.$adspaceId.'.'.$creativesId;
+        $this->publisherId = $publisherId;
+        $this->adSpaceId = $adSpaceId;
+    }
 
+    /**
+     * ex.: https://luna.r.lafamo.com/ts/
+     *
+     * @param string $trackingBaseUrl
+     */
+    public function setTrackingBaseUrl(string $trackingBaseUrl): void
+    {
+        if(str_ends_with($trackingBaseUrl, '/')) {
+            $this->trackingBaseUrl = $trackingBaseUrl;
+            return;
+        }
+
+        $this->trackingBaseUrl = $trackingBaseUrl.'/';
+    }
+
+    /**
+     * ID des Werbemittels (z.B.: Deeplink)
+     *
+     *
+     * @param int $creativesId deeplinkAdmediaId
+     */
+    public function setCreativesId(int $creativesId): void
+    {
+        $this->creativesId = $creativesId;
+    }
+
+    /**
+     * Externe ID des Advertiser
+     * @param string $advertiserId
+     */
+    public function setAdvertiseId(string $advertiseId): void
+    {
+        $this->advertiseId = $advertiseId.'/';
+    }
+
+    /**
+     * Klick Referenz wenn nur eins da ist dann String wenn mehrere dann array (['ref1', 'ref2'])
+     *
+     * @param array|string|null $clickRef
+     */
+    public function setClickRef(array|string|null $clickRef): void
+    {
+        $this->clickRef = $clickRef;
+    }
+
+    public function getByDeeplink($deeplink): string
+    {
+        $url_params_array['amc'] = 'con.blbn.'.$this->publisherId.'.'.$this->adSpaceId.'.'.$this->creativesId;
 
         /**
          * Click Reference
          * Max.: 6
          */
-        if($clickRef != null){
-            if(is_array($clickRef)) {
-                foreach($clickRef as $key => $value){
+        if($this->clickRef != null){
+            if(is_array($this->clickRef)) {
+                foreach($this->clickRef as $key => $value){
                     $keyForRef = $key+1;
                     $url_params_array['smc' . $keyForRef] = $value;
                 }
             }
             else{
-                $url_params_array['smc1'] = $clickRef;
+                $url_params_array['smc1'] = $this->clickRef;
             }
         }
 
@@ -55,6 +103,6 @@ class Belboon
 
         $url_params = http_build_query($url_params_array);
 
-        return $affiliate_domain.$advertiseId.'/tsc?'.$url_params;
+        return $this->trackingBaseUrl.$this->advertiseId.'tsc?'.$url_params;
     }
 }
